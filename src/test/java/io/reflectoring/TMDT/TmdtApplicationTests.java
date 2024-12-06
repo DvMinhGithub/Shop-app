e14e13b52bdee6fa5685d6e3e97d542959e375bd
Minh Dinh <minhdv.dev@gmail.com>
2024-12-06 11:27:25 +0700

    chore: Add initial Maven project

---
+++ src/test/java/io/reflectoring/TMDT/TmdtApplicationTests.java
@@ -0,0 +1,11 @@
+package io.reflectoring.TMDT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdtApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
